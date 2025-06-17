ddaceb55a70bb9b476c9c98b6fd66584282be1d5
manoj30dec <manojsoni.online@gmail.com>
2025-06-17 17:34:17 +0530

    data displayed on dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -2,13 +2,28 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { HttpService } from '../../services/http.service';
 import { Carousel } from '../../components/carousel/carousel';
 import { FeaturedList } from '../../components/featured-list/featured-list';
-
+import iPropertyDetail from "../../model/propertyDetail"
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'dashboard',
-  imports: [Carousel, FeaturedList],
+  imports: [Carousel, FeaturedList, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
 export class Dashboard {
+  private http = inject(HttpService)
+  // private endPoint = '/data/property-list.json';
+  private endPoint = "/assets/data/property-list.json";
+  listedProperty = signal<iPropertyDetail[] | undefined>(undefined);
 
+  ngOnInit() {
+    this.http.get<iPropertyDetail[]>(this.endPoint).subscribe({
+      next: res => {
+        console.log(res)
+        this.listedProperty.set(res)
+      },
+      error: err => console.error(err),
+    })
+
+  }
 }
